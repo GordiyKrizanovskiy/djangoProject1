f6a571d87419efe4967cba0085615c334283b626
gordi <gordiy.krizanovskiy@knu.ua>
2024-05-31 16:48:40 +0300

    add files

--- main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def about(request):
-    return render(request, 'main/about.html')
-
-def home_page(request):
-    return render(request, 'main/home_page.html')
